bd5fad019c32b49d5644467d9e2a2af7f5cabe08
Artem Merkulov <merkulov.a@svgun.ru>
2025-09-05 11:44:03 +0300

    feat: init repository

---
+++ src/base-config.js
@@ -0,0 +1,14 @@
+/**
+ * @type {import('prettier').Config}
+ */
+const baseConfig = {
+  singleQuote: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: "none",
+  arrowParens: "always",
+  printWidth: 80,
+  endOfLine: "lf",
+};
+
+export default baseConfig;
